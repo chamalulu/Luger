1fb7adfc20e8f07c5f84d4d503fa5057603388f0
Henrik Lundberg <henrik@chamalulu.com>
2018-05-10 17:14:15 +0200

    Add Mul64HiTest and fix overflow bug

--- Utilities.Tests/RNGTests.cs
+++ Utilities.Tests/RNGTests.cs
@@ -1,31 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
 using Xunit;
 
 namespace Luger.Utilities.Tests
 {
     public class IntExtTests
     {
-        private static BijectiveDictionary<ulong, long> testdata = BijectiveDictionary.Create<ulong, long>(
+        private static (ulong, long)[] as_testdata = new[]
+        {
             (0x0000_0000_0000_0000UL, 0),
             (0x7FFF_FFFF_FFFF_FFFFUL, long.MaxValue),
             (0x8000_0000_0000_0000UL, long.MinValue),
             (0xFFFF_FFFF_FFFF_FFFFUL, -1)
-        );
+        };
 
         [Fact]
         public void AsInt64Test()
         {
-            foreach (var (ul, l) in testdata)
+            foreach (var (ul, l) in as_testdata)
                 Assert.True(ul.AsInt64() == l);
         }
 
         [Fact]
         public void AsUInt64Test()
         {
-            foreach (var (ul, l) in testdata)
+            foreach (var (ul, l) in as_testdata)
                 Assert.True(l.AsUInt64() == ul);
         }
 
+        private const int multestlimit = 100;
 
+        private static IEnumerable<(ulong, ulong, ulong)> mul_testdata()
+            => from x in Enumerable.Range(0, multestlimit)
+               from y in Enumerable.Range(0, multestlimit)
+               let bix = ((BigInteger)x << 64) / multestlimit
+               let biy = ((BigInteger)y << 64) / multestlimit
+               let bia = bix * biy >> 64
+               select ((ulong)bix, (ulong)biy, (ulong)bia);
+
+        // private static IEnumerable<(ulong, ulong, ulong)> mul_testdata()
+        //     => new [] {(ulong.MaxValue, ulong.MaxValue, ulong.MaxValue-1)};
+
+        [Fact]
+        public void Mul64HiTest()
+        {
+            foreach (var (x, y, p) in mul_testdata())
+            {
+                var a = IntExt.Mul64Hi(x, y);
+                Assert.True(a == p);
+            }
+        }
     }
 
     public class RNGTests
--- Utilities/RNG.cs
+++ Utilities/RNG.cs
@@ -31,10 +31,19 @@ namespace Luger.Utilities
             /*
             x * y >> 64 = (xl + (xh << 32)) * (yl + (yh << 32)) >> 64
                         = xl * yl + xl * (yh << 32) + (xh << 32) * yl + (xh << 32) * (yh << 32) >> 64
-                        = (xl * yl + xl * (yh << 32) + (xh << 32) * yl >> 64) + xh * yh
-                        = ((xl * yl >> 32) + xl * yh + xh * yl >> 32) + xh * yh
             */
-            return ((xl * yl >> 32) + xl * yh + xh * yl >> 32) + xh * yh;
+            var acc1 = (xl * yl >> 32) + xl * yh;   // No risk of overflow
+            var hexacc1 = acc1.ToString("X");
+            var xhyl = xh * yl;
+            var hexxhyl = xhyl.ToString("X");
+            var acc2 = acc1 + xhyl; // Risk of overflow
+            var hexacc2 = acc2.ToString("X");
+            var carry = (acc1 ^ ((acc1 ^ xhyl) & (xhyl ^ acc2))) >> 31 & ~LoMask;
+            var hexcarry = carry.ToString("X");
+            var retval = (acc2 >> 32) + carry + xh * yh;  // No risk of overflow
+            var hexretval = retval.ToString("X");
+
+            return retval;
         }
     }
 
