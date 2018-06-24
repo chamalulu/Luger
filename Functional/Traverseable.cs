afe260427a2de6c9c530f7a2b34258cef8307a83
Henrik Lundberg <henrik@chamalulu.com>
2018-06-24 16:50:10 +0200

    Add imperative monadic traversal of transition over sequence

--- Functional/Traverseable.cs
+++ Functional/Traverseable.cs
@@ -8,7 +8,7 @@ namespace Luger.Functional
     public static class Traversable
     {
         /// <summary>
-        /// Monadic traversal of transition over sequence 
+        /// (Functional) Monadic traversal of transition over sequence 
         /// </summary>
         /// <param name="ts">Sequence to traverse over</param>
         /// <param name="f">Function mapping element to transition</param>
@@ -18,8 +18,30 @@ namespace Luger.Functional
         /// <returns>Transition yielding sequence results</returns>
         public static Transition<S, IEnumerable<R>> TraverseM<S, T, R>(this IEnumerable<T> ts, Func<T, Transition<S, R>> f)
             => ts.Aggregate(
-                new Transition<S, ImmutableList<R>>(s => (ImmutableList.Create<R>(), s)),
-                (trrs, t) => from rs in trrs from r in f(t) select rs.Add(r)
-            ).Map(list => list.AsEnumerable());
+                    new Transition<S, ImmutableList<R>>(s => (ImmutableList.Create<R>(), s)),
+                    (trrs, t) => from rs in trrs from r in f(t) select rs.Add(r)
+                ).Map(list => list.AsEnumerable());
+        
+        /// <summary>
+        /// (Imperative) Monadic traversal of transition over sequence
+        /// </summary>
+        /// <param name="ts">Sequence to traverse over</param>
+        /// <param name="f">Function mapping element to transition</param>
+        /// <typeparam name="S">Type of transition state</typeparam>
+        /// <typeparam name="T">Type of source element</typeparam>
+        /// <typeparam name="R">Type of transition result</typeparam>
+        /// <returns>Transition yielding sequence results</returns>
+        public static Transition<S, IEnumerable<R>> TraverseS<S, T, R>(this IEnumerable<T> ts, Func<T, Transition<S, R>> f)
+            => state =>
+            {
+                var source = ts.ToArray();
+                var length = source.Length;
+                var result = new R[length];
+
+                for(int i = 0; i < length; i++)
+                    (result[i], state) = f(source[i])(state);
+                
+                return (result, state);
+            };
     }
 }
