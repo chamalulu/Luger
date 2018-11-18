ea176e32e82263c9d5fb2dc631a6a89bc182586b
Henrik Lundberg <henrik@chamalulu.com>
2018-11-18 19:56:22 +0100

    Add Some<T> struct

--- src/Functional/Optional.cs
+++ src/Functional/Optional.cs
@@ -22,18 +22,38 @@ namespace Luger.Functional
 
         public static Optional<T> operator |(Optional<T> opt1, Optional<T> opt2)
             => opt1.IsSome ? opt1 : opt2;
-        
+
         public static T operator |(Optional<T> opt, T t)
             => opt.IsSome ? opt._value : t;
 
-        public string ToString(Func<T, string> formatter) => IsSome ? $"Some({formatter(_value)})" : "None"; 
-        
+        public string ToString(Func<T, string> formatter) => IsSome ? $"Some({formatter(_value)})" : "None";
+
         public override string ToString() => this.ToString(t => t.ToString());
+    }
+
+    public struct Some<T> where T : class
+    {
+        private readonly T _value;
+
+        private Some(T value)
+        {
+            _value = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public static explicit operator Some<T>(T value) => new Some<T>(value);
+
+        public static implicit operator T(Some<T> some) => some._value;
+
+        public static explicit operator Some<T>(Optional<T> opt) => new Some<T>(opt | null);
+
+        public static implicit operator Optional<T>(Some<T> some) => some._value;
+
+        public override string ToString() => this._value.ToString();
     }
 
     public static class Optional
     {
-        public class NonGenericNone {}
+        public class NonGenericNone { }
 
         public static NonGenericNone None => new NonGenericNone();
 
