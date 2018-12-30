a8b907ab7b4f14d048a105467f54ed1ee881eb67
Henrik Lundberg <henrik@chamalulu.com>
2018-12-30 15:33:33 +0100

    Reformat BijectiveDictionary

--- src/Utilities/BijectiveDictionary.cs
+++ src/Utilities/BijectiveDictionary.cs
@@ -11,29 +11,27 @@ namespace Luger.Utilities
 {
     public static class BijectiveDictionary
     {
-        public static BijectiveDictionary<TI, TCI> Create<TI, TCI>() =>
-            BijectiveDictionary<TI, TCI>.Empty;
+        public static BijectiveDictionary<TI, TCI> Create<TI, TCI>()
+            => BijectiveDictionary<TI, TCI>.Empty;
         
         public static BijectiveDictionary<TI, TCI> Create<TI, TCI>(
             IEqualityComparer<TI> itemComparer,
-            IEqualityComparer<TCI> coItemComparer) =>
-            BijectiveDictionary<TI, TCI>.Empty.WithComparers(itemComparer, coItemComparer);
+            IEqualityComparer<TCI> coItemComparer)
+            => BijectiveDictionary<TI, TCI>.Empty.WithComparers(itemComparer, coItemComparer);
         
         public static BijectiveDictionary<TI, TCI> ToBijectiveDictionary<TI, TCI>(
-            this IEnumerable<KeyValuePair<TI, TCI>> pairs) =>
-            pairs.Aggregate(
+            this IEnumerable<KeyValuePair<TI, TCI>> pairs)
+            => pairs.Aggregate(
                 BijectiveDictionary<TI, TCI>.Empty,
-                (bd, kvp) => bd.Add(kvp.Key, kvp.Value)
-                );
+                (bd, kvp) => bd.Add(kvp.Key, kvp.Value));
 
         public static BijectiveDictionary<TI, TCI> ToBijectiveDictionary<TI, TCI>(
             this IEnumerable<KeyValuePair<TI, TCI>> pairs,
             IEqualityComparer<TI> itemComparer,
-            IEqualityComparer<TCI> coItemComparer) =>
-            pairs.Aggregate(
+            IEqualityComparer<TCI> coItemComparer)
+            => pairs.Aggregate(
                 BijectiveDictionary<TI, TCI>.Empty.WithComparers(itemComparer, coItemComparer),
-                (bd, kvp) => bd.Add(kvp.Key, kvp.Value)
-                );
+                (bd, kvp) => bd.Add(kvp.Key, kvp.Value));
     }
 
     public class BijectiveDictionary<TI, TCI> : IEnumerable<(TI Item, TCI CoItem)>
@@ -52,28 +50,23 @@ namespace Luger.Utilities
 
         private BijectiveDictionary<TI, TCI> With(
             ImmutableDictionary<TI, TCI> itemDict,
-            ImmutableDictionary<TCI, TI> coItemDict
-            ) =>
-            object.ReferenceEquals(_itemDict, itemDict) && object.ReferenceEquals(_coItemDict, coItemDict)
+            ImmutableDictionary<TCI, TI> coItemDict)
+            => object.ReferenceEquals(_itemDict, itemDict) && object.ReferenceEquals(_coItemDict, coItemDict)
                 ? this
                 : new BijectiveDictionary<TI, TCI>(itemDict, coItemDict);
 
         public BijectiveDictionary<TI, TCI> WithComparers(
             IEqualityComparer<TI> itemComparer,
-            IEqualityComparer<TCI> coItemComparer
-            ) =>
-            object.ReferenceEquals(itemComparer, _itemDict.KeyComparer) && object.ReferenceEquals(coItemComparer, _coItemDict.KeyComparer)
+            IEqualityComparer<TCI> coItemComparer)
+            => object.ReferenceEquals(itemComparer, _itemDict.KeyComparer) && object.ReferenceEquals(coItemComparer, _coItemDict.KeyComparer)
                 ? this
                 : new BijectiveDictionary<TI, TCI>(
                     _itemDict.WithComparers(itemComparer, coItemComparer),
-                    _coItemDict.WithComparers(coItemComparer, itemComparer)
-                    );
+                    _coItemDict.WithComparers(coItemComparer, itemComparer));
 
-        public static BijectiveDictionary<TI, TCI> Empty =
-            new BijectiveDictionary<TI, TCI>(
+        public static BijectiveDictionary<TI, TCI> Empty = new BijectiveDictionary<TI, TCI>(
                 ImmutableDictionary<TI, TCI>.Empty,
-                ImmutableDictionary<TCI, TI>.Empty
-                );
+                ImmutableDictionary<TCI, TI>.Empty);
 
         public IEnumerable<TI> Items => _itemDict.Keys;
 
@@ -85,43 +78,43 @@ namespace Luger.Utilities
 
         public TI this[TCI coItem] => _coItemDict[coItem];
 
-        public BijectiveDictionary<TI, TCI> Clear() =>
-            this.With(_itemDict.Clear(), _coItemDict.Clear());
+        public BijectiveDictionary<TI, TCI> Clear()
+            => this.With(_itemDict.Clear(), _coItemDict.Clear());
 
-        public BijectiveDictionary<TI, TCI> Add(TI item, TCI coItem) =>
-            this.With(_itemDict.Add(item, coItem), _coItemDict.Add(coItem, item));
+        public BijectiveDictionary<TI, TCI> Add(TI item, TCI coItem)
+            => this.With(_itemDict.Add(item, coItem), _coItemDict.Add(coItem, item));
 
-        public BijectiveDictionary<TI, TCI> AddRange(IEnumerable<KeyValuePair<TI, TCI>> pairs) =>
-            pairs.Aggregate(this, (bd, kvp) => bd.Add(kvp.Key, kvp.Value));
+        public BijectiveDictionary<TI, TCI> AddRange(IEnumerable<KeyValuePair<TI, TCI>> pairs)
+            => pairs.Aggregate(this, (bd, kvp) => bd.Add(kvp.Key, kvp.Value));
 
-        public BijectiveDictionary<TI, TCI> Remove(TI item) =>
-            this.With(_itemDict.Remove(item), _coItemDict.Remove(_itemDict[item]));
+        public BijectiveDictionary<TI, TCI> Remove(TI item)
+            => this.With(_itemDict.Remove(item), _coItemDict.Remove(_itemDict[item]));
 
-        public BijectiveDictionary<TI, TCI> Remove(TCI coItem) =>
-            this.With(_itemDict.Remove(_coItemDict[coItem]), _coItemDict.Remove(coItem));
+        public BijectiveDictionary<TI, TCI> Remove(TCI coItem)
+            => this.With(_itemDict.Remove(_coItemDict[coItem]), _coItemDict.Remove(coItem));
 
-        public BijectiveDictionary<TI, TCI> RemoveRange(IEnumerable<TI> items) =>
-            items.Aggregate(this, (bd, item) => Remove(item));
+        public BijectiveDictionary<TI, TCI> RemoveRange(IEnumerable<TI> items)
+            => items.Aggregate(this, (bd, item) => Remove(item));
 
-        public BijectiveDictionary<TI, TCI> RemoveRange(IEnumerable<TCI> coItems) =>
-            coItems.Aggregate(this, (bd, coItem) => Remove(coItem));
+        public BijectiveDictionary<TI, TCI> RemoveRange(IEnumerable<TCI> coItems)
+            => coItems.Aggregate(this, (bd, coItem) => Remove(coItem));
 
         public bool Contains(TI item) => _itemDict.ContainsKey(item);
 
         public bool Contains(TCI coItem) => _coItemDict.ContainsKey(coItem);
 
-        public Optional<TCI> OptGetValue(TI item) =>
-            _itemDict.TryGetValue(item, out var value)
+        public Optional<TCI> OptGetValue(TI item)
+            => _itemDict.TryGetValue(item, out var value)
                 ? Some(value)
                 : None;
 
-        public Optional<TI> OptGetValue(TCI coItem) =>
-            _coItemDict.TryGetValue(coItem, out var value)
+        public Optional<TI> OptGetValue(TCI coItem)
+            => _coItemDict.TryGetValue(coItem, out var value)
                 ? Some(value)
                 : None;
 
-        public IEnumerator<(TI Item, TCI CoItem)> GetEnumerator() =>
-            _itemDict.Select(kvp => (kvp.Key, kvp.Value)).GetEnumerator();
+        public IEnumerator<(TI Item, TCI CoItem)> GetEnumerator()
+            => _itemDict.Select(kvp => (kvp.Key, kvp.Value)).GetEnumerator();
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
