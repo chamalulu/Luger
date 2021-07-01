e5e5f3ed12fa31c6c48d49d8f90d9c3c8a953598
Henrik Lundberg <henrik@chamalulu.com>
2021-07-01 12:57:12 +0200

    Some refactoring of Maybe.cs
    Constrain T type parameter as notnull.
    Implement IEnumerable<T> on Maybe<T>.

--- src/Functional/Maybe.cs
+++ src/Functional/Maybe.cs
@@ -1,12 +1,10 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Luger.Functional
 {
-    // TODO: Add notnull constraint to T.
-    // TODO: Implement IEnumerable<T>.
-    public readonly struct Maybe<T> : IEquatable<Maybe<T>>
+    public readonly struct Maybe<T> : IEquatable<Maybe<T>>, IEnumerable<T> where T : notnull
     {
         private readonly T _value;
 
@@ -15,32 +13,20 @@ namespace Luger.Functional
         internal Maybe(T value)
         {
             _value = value;
-            IsSome = value != null;
+            IsSome = true;
         }
 
-        internal TR MatchInternal<TR>(Func<T, TR> some, Func<TR> none) => IsSome ? some(_value) : none();
+        public TR Match<TR>(Func<T, TR> some, Func<TR> none) => IsSome ? some(_value) : none();
 
-        public TR Match<TR>(Func<T, TR> some, Func<TR> none)
+        public static Maybe<T> operator &(Maybe<T> left, Maybe<T> right) => left.IsSome ? right : left;
 
-            => MatchInternal(
-                some ?? throw new ArgumentNullException(nameof(some)),
-                none ?? throw new ArgumentNullException(nameof(none)));
+        public static Maybe<T> operator |(Maybe<T> left, Maybe<T> right) => left.IsSome ? left : right;
 
-        public static Maybe<T> operator |(Maybe<T> maybe1, Maybe<T> maybe2) => maybe1.IsSome ? maybe1 : maybe2;
+        public static T operator |(Maybe<T> left, T right) => left.IsSome ? left._value : right;
 
-        public static T operator |(Maybe<T> maybe, T t) => maybe.IsSome ? maybe._value : t;
+        public string ToString(Func<T, string?> formatter) => IsSome ? $"Some({formatter(_value)})" : "None";
 
-        public string ToString(Func<T, string?> formatter)
-        {
-            if (formatter == null)
-            {
-                throw new ArgumentNullException(nameof(formatter));
-            }
-
-            return IsSome ? $"Some({formatter(_value)})" : "None";
-        }
-
-        public override string ToString() => ToString(t => t?.ToString());
+        public override string ToString() => ToString(t => t.ToString());
 
         private static readonly IEqualityComparer<T> ValueEqualityComparer = EqualityComparer<T>.Default;
 
@@ -52,73 +38,84 @@ namespace Luger.Functional
 
         public override bool Equals(object? obj) => obj is Maybe<T> other && Equals(other);
 
-        public override int GetHashCode()
+        public override int GetHashCode() => IsSome ? ValueEqualityComparer.GetHashCode(_value) : 0;
 
-            => IsSome
-                ? ValueEqualityComparer.GetHashCode(_value!)
-                : 0;
+        public IEnumerator<T> GetEnumerator()
+        {
+            if (IsSome)
+            {
+                yield return _value;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
         public static bool operator ==(Maybe<T> left, Maybe<T> right) => left.Equals(right);
 
-        public static bool operator !=(Maybe<T> left, Maybe<T> right) => !(left == right);
+        public static bool operator !=(Maybe<T> left, Maybe<T> right) => !left.Equals(right);
+
+        public static implicit operator Maybe<T>(T? value) => value is not null ? new(value) : default;
+
+        public static implicit operator T?(Maybe<T> value) => value.IsSome ? value._value : default;
     }
 
     public static class Maybe
     {
-        public static Maybe<T> None<T>() => default;
+        public static Maybe<T> None<T>() where T : notnull => default;
 
-        public static Maybe<T> Some<T>(T value) => new(value ?? throw new ArgumentNullException(nameof(value)));
+        public static Maybe<T> Some<T>(T value) where T : notnull => new(value);
 
-        public static T ValueUnsafe<T>(this Maybe<T> maybeT)
+        public static T ValueUnsafe<T>(this Maybe<T> maybeT) where T : notnull
 
-            => maybeT.MatchInternal(
+            => maybeT.Match(
                 some: t => t,
                 none: () => throw new InvalidOperationException());
 
         public static Maybe<TR> Apply<T, TR>(this Maybe<Func<T, TR>> maybeFunc, Maybe<T> maybeT)
+            where T : notnull
+            where TR : notnull
 
-            => maybeFunc.MatchInternal(
-                some: f => maybeT.MatchInternal(
+            => maybeFunc.Match(
+                some: f => maybeT.Match(
                     some: t => Some(f(t)),
                     none: None<TR>),
                 none: None<TR>);
 
-        public static Maybe<TR> Bind<T, TR>(this Maybe<T> maybeT, Func<T, Maybe<TR>> func)
-        {
-            if (func is null)
-            {
-                throw new ArgumentNullException(nameof(func));
-            }
+        public static Maybe<TR> Bind<T, TR>(this Maybe<T> maybeT, Func<T, Maybe<TR>> func) where T : notnull where TR : notnull
 
-            return maybeT.MatchInternal(
+            => maybeT.Match(
                 some: func,
                 none: None<TR>);
-        }
 
-        public static Maybe<TR> Map<T, TR>(this Maybe<T> maybeT, Func<T, TR> func)
+        public static Maybe<TR> Map<T, TR>(this Maybe<T> maybeT, Func<T, TR> func) where T : notnull where TR : notnull
 
-            => maybeT.MatchInternal(
+            => maybeT.Match(
                 some: t => Some(func(t)),
                 none: None<TR>);
 
-        public static Maybe<TR> Select<T, TR>(this Maybe<T> maybeT, Func<T, TR> func) => maybeT.Map(func);
+        public static Maybe<TResult> Select<TSource, TResult>(this Maybe<TSource> source, Func<TSource, TResult> selector)
+            where TSource : notnull
+            where TResult : notnull
 
-        public static Maybe<TR> SelectMany<T, TR>(this Maybe<T> maybeT, Func<T, Maybe<TR>> func) => maybeT.Bind(func);
+            => source.Map(selector);
 
-        public static Maybe<TR> SelectMany<T, TC, TR>(this Maybe<T> maybeT, Func<T, Maybe<TC>> func, Func<T, TC, TR> proj)
+        public static Maybe<TResult> SelectMany<TSource, TNext, TResult>(
+            this Maybe<TSource> source,
+            Func<TSource, Maybe<TNext>> selector,
+            Func<TSource, TNext, TResult> resultSelector)
+            where TSource : notnull
+            where TNext : notnull
+            where TResult : notnull
 
-            => maybeT.MatchInternal(
-                some: t => func(t).MatchInternal(
-                    some: tc => Some(proj(t, tc)),
-                    none: None<TR>),
-                none: None<TR>);
+            => source.Match(
+                some: s => selector(s).Match(
+                    some: n => Some(resultSelector(s, n)),
+                    none: None<TResult>),
+                none: None<TResult>);
 
-        public static Maybe<T> Where<T>(this Maybe<T> maybeT, Func<T, bool> func) => maybeT.Bind(t => func(t) ? maybeT : None<T>());
+        public static Maybe<TSource> Where<TSource>(this Maybe<TSource> source, Func<TSource, bool> predicate)
+            where TSource : notnull
 
-        public static IEnumerable<T> AsEnumerable<T>(this Maybe<T> maybeT)
-
-            => maybeT.MatchInternal(
-                some: EnumerableExt.Return,
-                none: Enumerable.Empty<T>);
+            => source.Bind(s => predicate(s) ? source : None<TSource>());
     }
 }
