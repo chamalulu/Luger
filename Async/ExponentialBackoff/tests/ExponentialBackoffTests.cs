0b7f164abf8a39c630f81d8b816c3f27eccfe0cd
Henrik Lundberg <henrik@chamalulu.com>
2023-01-04 01:47:10 +0100

    Refactor test

--- Async/ExponentialBackoff/tests/ExponentialBackoffTests.cs
+++ Async/ExponentialBackoff/tests/ExponentialBackoffTests.cs
@@ -4,28 +4,26 @@ namespace Luger.Async.ExponentialBackoff.Tests;
 
 public class ExponentialBackoffTests
 {
-    static Task NoDelay(TimeSpan backoffDelay, CancellationToken cancellationToken) => Task.CompletedTask;
-
     [Theory]
     [InlineData(0, 1)]
     [InlineData(1, 2)]
     [InlineData(2, 3)]
-    public async void EBOverFaultingWithNRetriesYieldsNPlusOneCalls(uint retries, uint expectedCalls)
+    public async Task NRetriesYieldsNPlusOneCalls(uint retries, uint expectedCalls)
     {
         uint actualCalls = 0;
 
-        Task<object> func()
+        Task func()
         {
-            actualCalls++;
-            return Task.FromException<object>(new Exception());
+            Interlocked.Increment(ref actualCalls);
+            return Task.FromException(new Exception());
         }
 
-        async Task<object> testCode()
+        async Task testCode()
 
             => await ExponentialBackoff
                 .Over(func)
-                .WithRetries(retries)
-                .WithCustomDelay(NoDelay);
+                .WithCustomDelay((d, ct) => Task.CompletedTask)
+                .WithRetries(retries);
 
         _ = await Assert.ThrowsAsync<Exception>(testCode);
 
