34182869f82f0b955f14349808e993360b64cd88
Henrik Lundberg <henrik@chamalulu.com>
2023-01-04 01:46:44 +0100

    Add test

--- Async/ExponentialBackoff/tests/ExponentialBackoffTests.cs
+++ Async/ExponentialBackoff/tests/ExponentialBackoffTests.cs
@@ -1,3 +1,5 @@
+using System.Collections.Immutable;
+
 namespace Luger.Async.ExponentialBackoff.Tests;
 
 public class ExponentialBackoffTests
@@ -28,5 +30,37 @@ public class ExponentialBackoffTests
         _ = await Assert.ThrowsAsync<Exception>(testCode);
 
         Assert.Equal(expectedCalls, actualCalls);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(2)]
+    public async Task MeanDelayScaledByFactor(int factor)
+    {
+        var delaysTicks = ImmutableArray.Create<long>();
+
+        Task delay(TimeSpan d, CancellationToken ct)
+        {
+            ImmutableInterlocked.Update(ref delaysTicks, (s, v) => s.Add(v), d.Ticks);
+
+            return Task.CompletedTask;
+        }
+
+        async Task testCode()
+
+            => await ExponentialBackoff
+                .Over(() => Task.FromException(new Exception()))
+                .WithBaseDelay((DelayTimeSpan)TimeSpan.FromTicks(1L))
+                .WithCustomDelay(delay)
+                .WithCustomRNG(() => 1d - Math.Exp(-1d))  // Always mean
+                .WithFactor((TimeScaleFactor)factor)
+                .WithRetries(2u);
+
+        _ = await Assert.ThrowsAsync<Exception>(testCode);
+
+        Assert.Equal(2, delaysTicks.Length);
+        Assert.Equal(1L, delaysTicks[0]);
+        Assert.Equal(factor, delaysTicks[1]);
     }
 }
